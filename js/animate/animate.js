cd86f1a572fe096f2016a0c1a89debbfd753608a
DoVanNhatVN94 <dovannhatdn94@gmail.com>
2022-01-23 23:27:48 +0700

    up3

--- js/animate/animate.js
+++ js/animate/animate.js
@@ -3,7 +3,7 @@
 $(window).on("scroll", function () {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 200) {
+    if (scroll >= 100) {
         $("#headerOurTeach").addClass("nav-fixed");
         $("#headerOurTeach").addClass("animate__fadeInDownBig");
     } else {
@@ -15,7 +15,7 @@ $(window).on("scroll", function () {
 $(window).on("scroll", function () {
     var scroll2 = $(window).scrollTop();
     let mang = ["1", '2', '3', '4', '5', '6', '7', '8'];
-    if (scroll2 >= 200) {
+    if (scroll2 >= 100) {
         for (var i = 0; i < 8; i++) {
             $(`#people${mang[i]}`).addClass("animate__fadeInLeft");
         }
@@ -24,7 +24,7 @@ $(window).on("scroll", function () {
 });
 $(window).on("scroll", function () {
     var scroll3 = $(window).scrollTop();
-    if (scroll3 >= 950) {
+    if (scroll3 >= 850) {
         $("#find_left").addClass("animate__fadeInLeft");
         $("#find_right").addClass("animate__fadeInLeftBig");
     }
@@ -32,7 +32,7 @@ $(window).on("scroll", function () {
 $(window).on("scroll", function () {
     var scroll2 = $(window).scrollTop();
     let mang = ["1", '2', '3', '4'];
-    if (scroll2 >= 1600) {
+    if (scroll2 >= 1500) {
         for (var i = 0; i < 8; i++) {
             $(`#footer${mang[i]}`).addClass("animate__fadeIn");
         }
